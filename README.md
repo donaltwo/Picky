03789858c44aabdc29161a6268b0b8a3ae78dbdf
donaltwo <155965788+donaltwo@users.noreply.github.com>
2024-04-14 13:29:08 +0000

    added files copied from local copy VSCODE

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
 # Picky
 Second Code Institute Project
+Table of Contents
+Introduction
+Preparation Phase
+Features
+Testing
+Validator Testing
+Table of Testing Features
+Lighthouse Scoring
+Known Issues
+Deployment
+Media
+Credits & Acknowledgements
+
+
+Introduction
+Title: Picky! 
+Picky allows users to click on 5 flavours sweet, sour, bitter, umami, savoury  and 5 textures liquid,crispy,creamy, soft  & hard to help them decide what they want, taking the indecisiveness away around meal time. 
+
+Audience, people with dietary requirements, neurodivergent people or anyone who is picky with what they like to eat.
+User Case 1 
+A person with dietary requirements, who wants to use Picky! to easily find meals that suit their taste preferences, so that they can confidently choose meals that align with their dietary needs without spending excessive time browsing through menus or recipes.
+User Case 2
+Neurodivergent people, often struggle with decision-making, especially when it comes to food choices. With Picky!, I hope to simplify the process for them by having them selecting preferred flavors and textures, allowing them make meal decisions more efficiently.
+User Case 3
+People often find themselves stuck in a cycle of indecision during mealtime. Picky! provides these users with a solution by offering a tailored selection of meals based  alleviating the stress of deciding what to eat next.
+
+Idea: Using JavaScript code a meal picker based on what flavour and texture you want to eat by having an interactable 
+
+Preperation Phase
+I made wireframes in Balsamiq to give a rough estimation of what i wanted the interface to look like.
+*SCREENSHOTS*
+Feautures
+Splash Screen
+Using Javascript I coded in Javascript and CSS q quick welcome message giving the user the name and fucntion of the web app.
+*SCREENSHOTS*
+Flavour Buttons
+All the five flavours sweet,sour,bitter,umami and savoury.
+*SCREENSHOTS*
+Texture Buttons
+Five testures proivided liquid, crispy, soft, hard and creamy.
+*SCREENSHOTS*
+Random button
+Proivdes user random meal from array. 
+*SCREENSHOTS*
+Show all button
+Displays all meals hosted on web app.
+Reset button
+Clears current filter selection
+*SCREENSHOTS*
+Form that allows users to submit their own food and have it tagged
+
+ Testing
+
+ Validator Testing 
+
+ Table of testing features
+
+ Lighthouse Scoring
+
+ Known Issues
+
+Deployment 
+I deployed this web app via GitHub Pages was by doing the following steps:
+
+Signing in to GitHub.
+Finding the repository.
+Navigating to the "Settings" option in the menu bar.
+Proceeding to the "Pages" tab in the sidebar on the left.
+Under "Source," select the "Master" branch from the dropdown menu.
+Click the save button to apply the changes.
+Once saved, the page will update, displaying the message: "Your site is ready to be published at [URL]."
+
+Media
+
+Credits & Acknowledgements
+Thanks to my Code Institute mentor Dick V for his feedback and for the below resources in shaping this project.
+
+Code Institute
+Love Maths Walkthrough Project
+Youtube Resources
+https://www.youtube.com/watch?v=tDqTXipQmBU
+https://www.youtube.com/watch?v=MOlaldp1Fv4
+W3Schools
+https://www.w3schools.com/howto/howto_js_filter_elements.asp
+https://www.w3schools.com/js/tryit.asp?filename=tryjs_event_onfocus
+https://www.w3schools.com/howto/howto_js_alert.asp
+freecodeCamp
+https://www.freecodecamp.org/news/javascript-array-of-objects-tutorial-how-to-create-update-and-loop-through-objects-using-js-array-methods/
+https://www.freecodecamp.org/news/css-button-style-hover-color-and-background
+https://www.freecodecamp.org/news/creating-a-random-meal-generator/
+codedamn
+https://codedamn.com/news/javascript/submit-form-with-javascript
+Mozilla Developer
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_objects
+https://www.tutorialspoint.com/css/css_animation_bounce.htm
+https://stackoverflow.com/questions/61995995/how-to-give-random-background-color-to-list-of-buttons
---
+++ script.js
@@ -0,0 +1,117 @@
+// Alias commonly used functions for improved legiblity
+const $ = selector => document.querySelector(selector);
+const $$ = selector => document.querySelectorAll(selector);
+
+// Splash Screen 
+document.addEventListener("DOMContentLoaded", () => {
+    const splash = $(".splash");
+    setTimeout(() => {
+        splash.classList.add("display-none");
+    }, 5000);
+
+    // Header 
+    const header = $(".header");
+    window.addEventListener("scroll", () => {
+        const top = window.scrollY;
+        console.log(top);
+        if (top >= 50) {
+            header.classList.add("active");
+        } else {
+            header.classList.remove("active");
+        }
+    });
+})
+
+// Filter Selection modified from W3Schools code
+function filterSelection(c) {
+    let x = $$(".filterDiv"); //Select all elements with class'filterDiv'
+    if (c === "all") { // If all is selected
+        x.forEach(element => {
+            element.classList.add("show"); // Add 'show; class to all elemtns
+        });
+    } else {
+        x.forEach(element => {
+            if (element.className.includes(c)) {
+                element.classList.add("show");
+            } else {
+                element.classList.remove("show");
+            }
+        });
+    }
+}
+// Food name array includes user sumbissons when randomising as well as hard coded list
+function addRandom() {
+    const allFoodDivs = document.querySelectorAll('.filterDiv')
+    const foodNames = [];
+    for (var i = 0; i < allFoodDivs.length; ++i) {
+        foodNames.push(allFoodDivs[i].innerText);
+    }
+    const randomFood = foodNames[Math.floor(Math.random() * foodNames.length)];
+    alert("Picky has chosen for you to have: " + randomFood);
+}
+// Add and remove class functions modified from W3Schools code
+function addClass(element, name) {
+    const arr = element.className.split(" ");
+    if (!arr.includes(name)) {
+        element.classList.add(name);
+    }
+}
+function removeClass(element, name) {
+    element.classList.remove(name);
+}
+// Add event listener to each button to handle filtering
+const buttonContainer = $("#allbuttons");
+const buttons = buttonContainer.getElementsByClassName("button");
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+        const current = $(".active");
+        current.classList.remove("active");
+        this.classList.add("active");
+    });
+}
+// Submit food form
+const foodForm = $("#foodForm");
+foodForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const foodName = document.getElementById("foodName").value;
+    const flavour = document.getElementById("mainFlavour").value;
+    const texture = document.getElementById("mainTexture").value;
+    const dietary = document.getElementById("dietaryCategory").value;
+    if (foodName === "" || flavour.length === 0 || texture.length === 0 || !dietary) {
+        alert("Write your food item and tag it appropiately.");
+    } else {
+        addFood(foodName, flavour, texture, dietary); // Call addFood() function to handle form submission
+    }
+});
+// Adding new food items using form
+function addFood(foodName, flavour, texture, dietary) {
+    const foodItem = {
+        name: foodName,
+        flavour: flavour,
+        texture: texture,
+        dietary: dietary,
+    };
+    // Get stored food items from local storage or initialize empty array
+    const storedFoods = JSON.parse(localStorage.getItem("newfood")) || [];
+    storedFoods.push(foodItem);
+    localStorage.setItem("newfood", JSON.stringify(storedFoods));
+    const message = "You have added the following meal!" +
+        "\nMeal: " + foodName +
+        "\nMain Flavours: " + flavour +
+        "\nMain Textures: " + texture +
+        "\nDietary Category: " + dietary;
+    alert(message);
+    renderFoodList(); // Render the updated food list
+}
+// Function to render the list of food items
+function renderFoodList() {
+    const storedFoods = JSON.parse(localStorage.getItem("newfood")) || [];
+    const foodContainer = $(".newfood");
+    foodContainer.innerHTML = '';
+    storedFoods.forEach(food => {
+        const foodElement = document.createElement('div');
+        foodElement.className = 'filterDiv ' + food.flavour + food.texture + food.dietary; // Apply relevant classes for filtering by class 'filterDiv'
+        foodElement.textContent = food.name;// Set the text content to food name
+        foodContainer.appendChild(foodElement);  // Append food element to container
+    });
+}
